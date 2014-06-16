7a589596a9264ebf024a3e2ec5de55434f21c581
Stefano Lattuada <ste.lattuada@gmail.com>
2014-06-16 11:34:45 +0200

    finishing touch

--- huffman_new.pl
+++ huffman_new.pl
@@ -1,16 +1,20 @@
+%generate_huffman_tree/3: generates tree from a list of symbol-weight couples.
 generate_huffman_tree(SymbolsAndWeights, HuffmanTree) :- 
 	initialize_tree(SymbolsAndWeights, SymbolsAndWeightsTree),
 	generate_huffman_tree_aux(SymbolsAndWeightsTree, HuffmanTree).
-
-initialize_tree([[Symbol, Weight]|SymbolsAndWeights], SymbolsAndWeightsTree) :-
-	initialize_tree(SymbolsAndWeights, Temp),
-	append([t([[Symbol], Weight], nil, nil)], Temp, SymbolsAndWeightsTree).
-initialize_tree([],[]) :-!.
 
 generate_huffman_tree_aux(SymbolsAndWeightsTree, HuffmanTree) :-
 	insert_sort(SymbolsAndWeightsTree, Ordered),	
 	generate_tree(Ordered, HuffmanTree).
 
+
+%initialize_tree/2: formats symbol-weight list in a leaf-only tree.
+initialize_tree([[Symbol, Weight]|SymbolsAndWeights], SymbolsAndWeightsTree) :-
+	initialize_tree(SymbolsAndWeights, Temp),
+	append([t([[Symbol], Weight], nil, nil)], Temp, SymbolsAndWeightsTree).
+initialize_tree([],[]).
+
+%generate_tree/2: huffman algorithm for tree generation.
 generate_tree([A,B|Ordered], Tree) :-
 	unite(A, B, United),
 	append([United], Ordered, OrderedFinal),
@@ -18,22 +22,12 @@ generate_tree([A,B|Ordered], Tree) :-
 	!.
 generate_tree([A|[]], A) :- !.
 
+%unite/3: unites two nodes.
 unite(t([SymbolA, WeightA], RightA, LeftA), t([SymbolB, WeightB], RightB, LeftB), t([SymbolAB, WeightAB], t([SymbolA, WeightA], RightA, LeftA), t([SymbolB, WeightB], RightB, LeftB))) :-
 	append(SymbolA, SymbolB, SymbolAB),
 	WeightAB is WeightA + WeightB.
 
-sort_rule(Delta, t([_,WeightA],_,_), t([_, WeightB],_,_)) :-
-	compare(Delta, WeightA, WeightB).
-
-insert_sort(List,Sorted):-i_sort(List,[],Sorted).
-i_sort([],Acc,Acc).
-i_sort([H|T],Acc,Sorted):-insert(H,Acc,NAcc),i_sort(T,NAcc,Sorted).
-   
-insert(X,[Y|T],[Y|NT]):-sort_rule(>, X, Y),!,insert(X,T,NT).
-insert(X,[Y|T],[X,Y|T]):-sort_rule(<, X, Y),!.
-insert(X,[],[X]).
-
-
+%decode/3: decodes a list of bits into a message following an huffman tree.
 decode(Bits, HuffmanTree, Message) :-
 	decode_aux(Bits, HuffmanTree, Character, RemainingBits),
 	decode(RemainingBits, HuffmanTree, Rest),
@@ -53,6 +47,7 @@ decode_aux([1|Bits], HuffmanTree, Message, RemainingBits) :-
 	!,
 	decode_aux(Bits, LeftTree, Message, RemainingBits).
 
+%encode/3: encodes a message in a list of bits following an huffman tree.
 encode([Char|RemainingMessage], HuffmanTree, Bits) :-
 	encode_aux(Char, HuffmanTree, Code),
 	encode(RemainingMessage, HuffmanTree, Rest),
@@ -71,12 +66,26 @@ encode_aux(Char, t(_,_,t([Symbol, Weight], LeftTree, RightTree)), Code) :-
 	append([0], Rest, Code).
 encode_aux(Char, t([[Char], _], nil, nil), []).
 
-right_tree(t(_,_,RightTree), RightTree).
-left_tree(t(_,LeftTree,_), LeftTree).
-
+%generate_symbol_bits_table/2: generates a table of couples consisting of a symbol and its relative code.
 generate_symbol_bits_table(t([[Char|Symbol], Weight],LeftTree ,RightTree), SymbolBitsTable) :-
 	encode([Char], t([[Char|Symbol], Weight],LeftTree ,RightTree), Code),
 	!,
 	generate_symbol_bits_table(t([Symbol, Weight],LeftTree ,RightTree), Rest),
 	append([[Char, Code]], Rest, SymbolBitsTable).
 generate_symbol_bits_table(t([[],_], _, _), []).
+
+
+%insert_sort/2: insertion sort implementation for nodes.
+insert_sort(List,Sorted):-i_sort(List,[],Sorted).
+i_sort([],Acc,Acc).
+i_sort([H|T],Acc,Sorted):-insert(H,Acc,NAcc),i_sort(T,NAcc,Sorted).
+   
+insert(X,[Y|T],[Y|NT]):-sort_rule(>, X, Y),!,insert(X,T,NT).
+insert(X,[Y|T],[X,Y|T]):-sort_rule(<, X, Y),!.
+insert(X,[],[X]).
+
+sort_rule(Delta, t([_,WeightA],_,_), t([_, WeightB],_,_)) :-
+	compare(Delta, WeightA, WeightB).
+
+right_tree(t(_,_,RightTree), RightTree).
+left_tree(t(_,LeftTree,_), LeftTree).
